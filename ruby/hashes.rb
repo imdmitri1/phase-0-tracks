1bfac006431220d19f8e06e363c8a2c4e66585a2
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-04-26 17:58:31 -0500

    write designer program

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,11 +2,12 @@
 #create empty hash
 #ask for the name, age, kids, fav color, if ready to book
 #ask if want to add smth else
-#somehow ask to correct mistakes
+#ask to somehow correct mistakes
 
 puts "hello! do we have a client?"
 client=gets.chomp
 application = {name:"", age:"", kids:"", color:"" }
+#filling out form if yes/y
 if client == "yes" || client == "y"
  puts "Name:"
  application[:name] = gets.chomp
@@ -21,18 +22,19 @@ if client == "yes" || client == "y"
  application[:color] = gets.chomp.to_s
    #printing whole hash
    puts application
-   puts "would you like to add smth else (y/n)?"
+   #asking for update
+   puts "would you like to update something (y/n)?"
    if gets.chomp == "y" 
+    #asking for key
     puts "please enter description name:"
     mistake_key = gets.chomp.to_sym
+    #asking for value
     puts "please enter description:"
     mistake_value = gets.chomp
     application[mistake_key] = mistake_value
     puts application
-else puts"thanks for filling out!"
-end
-
-
-
+    else puts"thanks for filling out!"
+    end
+#saying BYE if answer "no" at the beginning
 else puts"Good bye!"
 end
