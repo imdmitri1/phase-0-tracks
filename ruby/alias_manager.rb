a30609a3ee933a9a253b95ae6fe4c52d4d06a856
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-04-27 18:46:26 -0500

    split in 3 dif methods

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,38 +1,50 @@
 #ask for the first and last name
 #swap names
 #change vowels to the next one in "aeiou"
-#and consonants to the next one except "aeiou"
+#and consonants to the next one in alphabet except "aeiou"
 
-def fake_name
-    #changing name places and splitting
-    puts
-    str_name = "Felicia Torres"
-    str_name = str_name.split(' ').reverse
-    vowels = ["a","e","i","o","u","a"]
+def letters_change(arr)
+        vowels = ["a","e","i","o","u","a"]
     consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","b"]
+    n = -1
+        until (arr.length) * -1 > n 
+      if vowels.include?(arr[n])
+          arr[n] = vowels[vowels.index(arr[n]) + 1]
+      elsif consonants.include?(arr[n])
+          arr[n] = consonants[consonants.index(arr[n]) + 1]
+      end 
+      n -=1 
+  end
+  arr
+end
+#***********************************************
+def name_change(str_name)
+    #changing name places and splitting
+    str_name = str_name.split(' ').reverse
+    r_name1 = str_name[0].downcase.split('')
+    r_name2 = str_name[1].downcase.split('')
     f_name1 = []
     f_name2 = []
-    name1 = str_name[0].downcase.split('')
-    name2 = str_name[1].downcase.split('')
-    n = -1
-    until (name1.length) * -1 > n 
-                      if vowels.include?(name1[n])
-                      name1[n] = vowels[vowels.index(name1[n]) + 1]
-                    elsif consonants.include?(name1[n])
-                      name1[n] = consonants[consonants.index(name1[n]) + 1]
-                    end 
-                    n -=1 
-             end 
-    n1 = -1
-    until (name2.length) * -1 > n1 
-                      if vowels.include?(name2[n1])
-                      name2[n1] = vowels[vowels.index(name2[n1]) + 1]
-                    elsif consonants.include?(name2[n1])
-                      name2[n1] = consonants[consonants.index(name2[n1]) + 1]
-                    end 
-                    n1 -=1 
-             end 
-    p name1
-    p name2
+    f_name1 << letters_change(r_name1)
+    f_name2 << letters_change(r_name2)
+  f_name = f_name1.join.capitalize + " " + f_name2.join.capitalize 
+  f_name
 end
-fake_name
+#***********************************************
+def fake_name_for_user
+    puts "Hey, lets get you a fake(coded) name! What is your first and last name?"
+    user_input = gets.chomp
+    all_names = {}
+    until user_input == "quit"
+    f_name = name_change(user_input)
+    all_names[user_input] = f_name
+    puts "Well this would be: #{f_name}"
+    puts "If you don't like it, give us another name(type 'quit', when finished):"
+    user_input = gets.chomp
+    end
+    puts "Here are all the names you tried: #{all_names}"
+    puts "Thanks for using and good luck!"
+end
+
+fake_name_for_user
+
