0829e5f05bd5d089140a16d5c58b90ee35922755
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-04-27 19:00:31 -0500

    add decsription

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,12 +4,15 @@
 #and consonants to the next one in alphabet except "aeiou"
 
 def letters_change(arr)
+    #arrays of all vowels and consonants
         vowels = ["a","e","i","o","u","a"]
     consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","b"]
     n = -1
         until (arr.length) * -1 > n 
+     #changing vovels
       if vowels.include?(arr[n])
           arr[n] = vowels[vowels.index(arr[n]) + 1]
+     #changing consonants
       elsif consonants.include?(arr[n])
           arr[n] = consonants[consonants.index(arr[n]) + 1]
       end 
@@ -25,23 +28,30 @@ def name_change(str_name)
     r_name2 = str_name[1].downcase.split('')
     f_name1 = []
     f_name2 = []
+    #inserting modified letters in new arrays
     f_name1 << letters_change(r_name1)
     f_name2 << letters_change(r_name2)
+    #converting to a single string
   f_name = f_name1.join.capitalize + " " + f_name2.join.capitalize 
   f_name
 end
 #***********************************************
 def fake_name_for_user
+    #asking for name
     puts "Hey, lets get you a fake(coded) name! What is your first and last name?"
     user_input = gets.chomp
+    #empty hash to collect all names
     all_names = {}
+    #loop for more names
     until user_input == "quit"
     f_name = name_change(user_input)
+    #collecting all names
     all_names[user_input] = f_name
     puts "Well this would be: #{f_name}"
     puts "If you don't like it, give us another name(type 'quit', when finished):"
     user_input = gets.chomp
     end
+    #printing name collection
     puts "Here are all the names you tried: #{all_names}"
     puts "Thanks for using and good luck!"
 end
