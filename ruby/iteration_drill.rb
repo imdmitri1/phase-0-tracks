586d4a0570a36e5b55c6bdba4824dc45bb3d66af
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-04-28 19:04:20 -0500

    add rest of the methods/finish

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -78,6 +78,11 @@ end
 # documentation for Arrays.
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
+
+
+zombie_apocalypse_supplies.concat(other_survivor_supplies).uniq!
+
+
 # ----
 
 # Hash Drills
@@ -94,16 +99,58 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
+
+extinct_animals.each {|k,v| puts "#{k} - #{v} *"}
+
 # ----
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
+
+
+#option #1 but uses .each
+extinct_animals.each do |k,v| if v > 2000
+                               # extinct_animals.delete(k)   #commented for no damage to hash
+                            end
+                    end
+puts extinct_animals
+
+#option #2 
+
+def keeptill(h,x)
+    i = 0 
+    a1 = []
+    a2 = []
+    a1 << h.values                  #didn't know at that point that 
+    a2 << h.keys                    # I can use indexes on h.keys
+    while h.length > i
+            if   a1[0][i] > x
+                h.delete(a2[0][i])
+            end
+    i += 1 
+    end 
+puts h
+end
+#keeptill(extinct_animals,1997)
+
+
 # ----
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
+
+def keeptill(h,x)
+            h.each do |k,v| if v > x
+                            h.delete(k)
+                            end
+                    end
+puts h
+end
+        #keeptill(extinct_animals,1997)
+
+
 # ----
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
@@ -112,10 +159,32 @@ extinct_animals = {
 # "Dodo"
 # "Saiga Antelope"
 # Do not use any special built-in methods.
+
+def includedin(h,k)
+    i = 0 
+    nomatch = true
+    until i > h.length
+        if h.keys[i] == k
+            puts "yes, it is included: #{k}"
+            nomatch = false
+        and
+    i +=1
+    end
+    if nomatch == true ; puts "no match" ; end
+end
+includedin(extinct_animals,"Andean Cat")
+includedin(extinct_animals,"Dodo")
+includedin(extinct_animals, "Saiga Antelope")
+
+
+
 # ----
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
+
+extinct_animals.delete_if {|k,v| k == "Passenger Pigeon"}.flatten
+
 # ----
