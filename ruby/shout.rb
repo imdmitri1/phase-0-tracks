1659b6484ad5ecb4bfd5980e7339a856b241a39f
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-05-02 17:46:47 -0500

    add module shot

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-    
+
+    def self.yell_angrily(word) 
+    word + "!!!" + " :("
+    end
+
+    def self.yell_happily(word)
+    word.upcase + "!!!!!" + " :)))"
+    end
 end
+
+shout.yell_angrily("abra-cadabra")
+shout.yell_happily("abra-cadabra")
