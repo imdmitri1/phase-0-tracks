43f8492eb437b089f02372662c486fe99f4c7e55
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-05-02 18:49:16 -0500

    add mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,36 @@
+# module Shout
+
+#     def self.yell_angrily(word) 
+#     word + "!!!" + " :("
+#     end
+
+#     def self.yell_happily(word)
+#     word.upcase + "!!!!!" + " :)))"
+#     end
+# end
+
+
 module Shout
-
-    def self.yell_angrily(word) 
-    word + "!!!" + " :("
+    def happy(name)
+        puts "#{name} is super happy today! ohh-yeaaaa!!!"
     end
-
-    def self.yell_happily(word)
-    word.upcase + "!!!!!" + " :)))"
+    def sad(name)
+        puts "#{name} is sad at the moment, lets make him SMILEeee!!!"
     end
 end
 
-shout.yell_angrily("abra-cadabra")
-shout.yell_happily("abra-cadabra")
+class Coworker
+    include Shout
+end
+
+class Boss
+    include Shout
+    end
+
+today = Coworker.new
+today.happy("John")
+today.sad("Bill")
+
+yesterday = Boss.new
+yesterday.happy("Anna")
+yesterday.sad("Carl")
