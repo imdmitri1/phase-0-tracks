5b36ed356205d9ef32227babfe762917208cd824
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-05-05 08:58:09 -0500

    add guess_word method

--- ruby/guess_word_game/guess_word_game.rb
+++ ruby/guess_word_game/guess_word_game.rb
@@ -1,9 +1,11 @@
 class Wordgame
+    attr_reader :hidden_word
     attr_writer :letter
+
 
     def initialize
         @word = [] 
-        @letter = letter
+        @letter = ""
         @hidden_word = []
     end
 
@@ -16,13 +18,34 @@ class Wordgame
 
     def guess_letter(letter)
         if @word.include?(letter)
-            @hidden_word[@word(letter)] = letter
+            puts "You got it"
+            @hidden_word[@word.index(letter)] = letter
+        #elsif                  #add repetitive letters
+                
         else puts "wrong guess"
         end
-    @hidden_word        
+            @hidden_word.join  
     end
 
-    def guess_word
-        
+    def guess_word(word)
+        if @word.join == word
+            puts "You got whole word!!! Congrats!"
+            true
+        else puts "wrong"
+            false
+        end        
     end
 end
+
+#DRIVER CODE ----------------------------------------------
+game = Wordgame.new
+#p game
+p "*"*50
+p game.hide_word("a massage")
+p "*"*50
+p game.guess_letter("s")
+p "*"*50
+p game.hidden_word
+p "*"*50
+p game.guess_word("a massage")
+
