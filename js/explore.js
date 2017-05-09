1eb00076fd93d6fbf6dde32821dcd64e6a2aad0a
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-05-09 10:08:18 -0500

    add reverse and if/else statement functions

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,29 @@
 //  taking the string and creating loop for the
-// length of the string, each step cutting last 
-// letter of the string and adding to new variable 
+// length of the string, each step takes last 
+// letter of the string and adding to a new variable 
+
+
+
+function reverse(str) {
+    var b = ""
+    for (var i = str.length -1; i >= 0; i--) {
+        b = b+str[i]
+}
+    console.log(b)
+    return b
+}
+
+//driver code:
+reverse("hello")
+reverse("stressed")
+
+//checking if palindrome
+ function palindromeCheck(str){
+    if (str == reverse(str)) {
+        console.log( str + " is palindrome!");
+    } else { console.log(str + " is not palidrome!");
+    }
+ }
+
+palindromeCheck("hahah")
+palindromeCheck("rainy day")
