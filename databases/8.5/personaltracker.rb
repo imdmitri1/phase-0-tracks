2d6d5c84d9fdbde8903afea38518ca5b51bef65c
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-05-18 09:19:37 -0500

    pseudo code

---
+++ databases/8.5/personaltracker.rb
@@ -0,0 +1,7 @@
+# Personal Tracker
+# pseudo code:
+# offer the output or input of the info
+# Ask for all the input info (work out, food, sleep, mood)
+# store it in appropriate tables
+# create output for desire amount of days
+
