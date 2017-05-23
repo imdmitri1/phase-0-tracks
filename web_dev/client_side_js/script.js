b23259bfd324a8bca0c7084c6f40f43ee56858f4
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-05-23 09:41:46 -0500

    add files and some code + many errors

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,29 @@
+// console.log("JavaScript is running");
+
+// var table =document.getElementsByTagName("table");
+
+// table.hidden = true;
+
+// // table.border = "5px solid black"
+
+// var bg = document.getElementByName("a1-1.jpg");
+// bg.hidden = true;
+
+
+// var welcome = document.getElementsByTagName("h3")
+// welcome.style.color: "red";
+
+var photo = document.getElementById("lizard-photo");
+photo.style.border = "3px solid red"
+
+
+
+// var par = document.getElementsByTagName("p");
+// par.style.fontSize = "xx-large";
+// par.style.align = "center"
+
+var body = document.getElementById("about-my-pet");
+body.style.backgroundColor = "grey";
+
+var list = document.getElementsByName("li");
+list.style.color = "red";
