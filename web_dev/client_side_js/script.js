157086e679f8f0646d08fd1a7fe902946a37fe0e
Dmitri Stefanisin <dmitri.stefanisin@gmail.com>
2017-05-23 10:26:27 -0500

    add event listeners and more errors / finish

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -25,5 +25,53 @@ photo.style.border = "3px solid red"
 var body = document.getElementById("about-my-pet");
 body.style.backgroundColor = "grey";
 
-var list = document.getElementsByName("li");
-list.style.color = "red";
+var list = document.getElementsByTagName("ul");
+list
+var line = list[1];
+line
+// line.style.border = "3px solid red";
+
+// var a = document.getElementsByClassName("a1");
+// a.style.color = "blue";
+
+var newp = document.createElement('p');
+var newptext = document.createTextNode("some text here");
+newp.appendChild(newptext);
+
+
+// release 2 .................................
+
+function addblueborder(event){
+    console.log("happened");
+    console.log(event);
+    event.target.style.border = "5px dashed blue";
+}
+
+var picture = document.getElementById("lizard-photo");
+photo.addEventListener("mouseover", addblueborder);
+
+function gobackborder(event){
+    console.log("happened");
+    console.log(event);
+    event.target.style.border = "3px solid red";
+}
+var picture = document.getElementById("lizard-photo");
+photo.addEventListener("mouseout", gobackborder);
+
+//................
+
+function bgcolor(event){
+    // console.log(event);
+    event.target.style.backgroundColor = "purple";
+}
+var bg = document.getElementById("about-my-pet");
+bg.addEventListener("dblclick", bgcolor);
+// bg.addEventListener("mouseenter", bgcolor);
+
+function bg2color(event){
+    // console.log(event);
+    event.target.style.backgroundColor = "light grey";
+}
+bg.addEventListener("mouseleave", bg2color);
+
+
